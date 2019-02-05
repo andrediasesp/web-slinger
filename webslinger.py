fe527de240a636e212a020244a2b9ee9f1bf75df
Andre Dias <andrepldias@hotmail.com>
2019-02-05 12:46:46 +0000

    Converting returning list to dict.

--- webslinger.py
+++ webslinger.py
@@ -3,12 +3,28 @@ import downloading
 import crawlers
 import scraping
 import pymongo
+import itertools
 
 
-def run_slinger():
+
+def run_slinger(url):
+    """
     # Make a MongoDB connection instance
     client = pymongo.MongoClient("mongodb://localhost:27017/")
     database = client["promotion_info"]
     print(client.list_database_names())
     if "promotion_info" in dblist:
         print("The database exists.")
+    """
+    data = crawlers.crawl_website_ID(url)
+    id = 0
+    dict = {}
+    for a,b in data:
+        dict[id] = {'game': a, 'price': b}
+        id += 1
+
+def main():
+    run_slinger('https://www.worten.pt/promocoes?categoria=Gaming%20e%20Entretenimento&tipologia=Jogos%20PS4&page=')
+
+if __name__ == '__main__':
+    main()
