c9f1d0359ec13b349bd5972c3dcd1530ff2d4119
Andre Dias <andrepldias@hotmail.com>
2019-02-05 12:45:09 +0000

    Editing and cleaning Crawlers file

--- crawlers.py
+++ crawlers.py
@@ -7,27 +7,29 @@ from urllib.parse import urljoin
 import scraping
 
 def crawl_website_sitemap(url):
-    """ Crawling websites sitemap."""
+    """ Crawls websites sitemap. Doesn't cover sitemaps gz files """
     # Crawl a given website's sitemap
     sitemap = downloading.download_webpage(url)
     # Extract all links from a given sitemap - lazy mode
     site_links = re.findall(r'<loc>(.*?)</loc>',sitemap)
     for link in site_links:
         html_page = downloading.download_webpage(link)
-        time.sleep(3)
-        #print(html_page)
+        time.sleep(2)
 
 def crawl_website_ID(url, max_errors = 3):
-    """ Crawler based on database ID Links. Maximum 3 consecutive non-existing webpages to break execution """
+    """ Crawler based on database ID Links. Maximum 3 consecutive
+        non-existing webpages to break execution. Calls scraping method to
+        retrieve info from website pages.
+        Returns a List of all the information found across all pages scrapped
+    """
     seen = set(url)
-    #for id in itertools.count(1):
     info = []
-    for id in range(11):
+    for id in itertools.count(1):
         page_id = '{}{}'.format(url,id)
         if page_id not in seen:
             seen.add(page_id)
             html_page = downloading.download_webpage(page_id)
-            # 3 seconds delay between request, don't want to overload the server
+            # 2 seconds delay between requests, don't want to overload the server
             time.sleep(2)
             if html_page is None:
                 num_errors += 1
@@ -38,10 +40,3 @@ def crawl_website_ID(url, max_errors = 3):
                 soup = scraping.soup_html(html_page)
                 info.extend(scraping.search_soup(soup,info))
     return info
-
-def main():
-    #crawl_website_sitemap('http://example.webscraping.com/sitemap.xml')
-    data = crawl_website_ID('https://www.worten.pt/promocoes?categoria=Gaming%20e%20Entretenimento&tipologia=Jogos%20PS4&page=')
-    print(data)
-if __name__ == '__main__':
-    main()
