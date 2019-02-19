d410f32049f4e9d34e55b3e2f10bc16edd44e022
Andre Dias <andrepldias@hotmail.com>
2019-02-19 14:16:37 +0000

    Adding MongoDB Driver to store results

--- webslinger.py
+++ webslinger.py
@@ -3,25 +3,25 @@ import downloading
 import crawlers
 import scraping
 import pymongo
+from datetime import datetime
 import itertools
 
 
 
 def run_slinger(url):
-    """
+
     # Make a MongoDB connection instance
     client = pymongo.MongoClient("mongodb://localhost:27017/")
-    database = client["promotion_info"]
-    print(client.list_database_names())
-    if "promotion_info" in dblist:
+    database = client["crawling"]
+    dblist = client.list_database_names()
+    if "crawling" in dblist:
         print("The database exists.")
-    """
+    # Crawl url to retrieve PS4 games in promotion and their price
     data = crawlers.crawl_website_ID(url)
-    id = 0
     dict = {}
     for a,b in data:
-        dict[id] = {'game': a, 'price': b}
-        id += 1
+        prom = {'game': a, 'price': b, 'insert_date': datetime.now()}
+        result = database.promotions.insert_one(prom)
 
 def main():
     run_slinger('https://www.worten.pt/promocoes?categoria=Gaming%20e%20Entretenimento&tipologia=Jogos%20PS4&page=')
